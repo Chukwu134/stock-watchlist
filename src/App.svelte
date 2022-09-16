847508c771ac277e6fac111d6febde52b2f5da29
Chukwuemeka Ubakanma <emeka@dhcp-vl2041-32455.eduroam.cornell.edu>
2022-09-16 13:25:06 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+	export let name;
+	import Login from './Login.svelte';
+
+	let user = { loggedIn: true };
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+</script>
+
+<!-- Create Text field for username -->
+
+  
+
+
+<main>
+	<h1>Hello, Welcome to {name}!</h1>
+	<p>Please login below</p>
+</main>
+
+
+
+
+
+
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{:else}
+	
+	<Login/>
+{/if}
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Login.svelte
@@ -0,0 +1,7 @@
+<form>
+	<label for="user">Username:</label><br>
+	<input type="text" id="user" name="user"><br>
+    <label for="pass">Password:</label><br>
+	<input type="text" id="pass" name="pass"><br>
+	<input type="submit" value="Log in">
+</form>
